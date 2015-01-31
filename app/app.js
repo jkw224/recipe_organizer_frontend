2b590b63c1a752484d4cf39399f094332175e150
Jonathan Wood <jonathankimballwood@gmail.com>
2015-01-30 22:32:04 -0700

    added gulp -- program works

--- app/app.js
+++ app/app.js
@@ -3,10 +3,15 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.recipes',
+  'myApp.addRecipe',
+  'myApp.version',
+  'restangular'
+
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$routeProvider', function($routeProvider, RestangularProvider) {
+  $routeProvider.otherwise({redirectTo: '/recipes'});
+
+  RestangularProvider.setBaseUrl('http://localhost:8001')
+
 }]);
